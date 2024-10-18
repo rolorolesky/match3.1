0878cf0163054a10dc32406fb8d3e8292595a2b9
rolorolesky <rolorolesky@outlook.es>
2024-10-18 05:27:52 -0300

    clase 14

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Board : MonoBehaviour
 {
@@ -11,14 +12,40 @@ public class Board : MonoBehaviour
     public float cameraSizeOffset;
     public float cameraVerticalOffset;
 
+    public GameObject[] availablePieces;
+
+    Tile[,] Tiles;
+    Piece[,] Pieces;
+
+    Tile startTile;
+    Tile endTile;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Tiles = new Tile[width, height];
+        Pieces = new Piece[width, height];
+
         SetupBoard();
         PositionCamera();
+        SetupPieces();
 
 
+    }
+    private void SetupPieces()
+        {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var selectedPiece = availablePieces[UnityEngine.Random.Range(0, availablePieces.Length)];
+                var o = Instantiate(selectedPiece, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
+                Pieces[x, y] = o.GetComponent<Piece>();
+                Pieces[x, y].Setup(x, y, this);
+            }
+        }
     }
     private void PositionCamera()
     {
@@ -39,13 +66,50 @@ public class Board : MonoBehaviour
             {
                 var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
+                Tiles[x, y] = o.GetComponent<Tile>();
+                Tiles[x, y]?.Setup(x, y, this);
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TileDown(Tile tile_)
     {
-        
+        startTile = tile_;
+    }
+    public void TileOver(Tile tile_)
+    {
+        endTile = tile_;
+    }
+    public void TileUp(Tile tile_)
+    {
+        if(startTile !=null && endTile !=null && IsCloseTo(startTile, endTile))
+        {
+            SwapTiles();
+        }
+        startTile = null;
+        endTile = null;
+    }
+    private void SwapTiles()
+    {
+        var StartPiece = Pieces[startTile.x, startTile.y];
+        var EndPiece = Pieces[endTile.x, endTile.y];
+
+        StartPiece.Move(endTile.x, endTile.y);
+        EndPiece.Move(startTile.x, startTile.y);
+
+        Pieces[startTile.x, startTile.y] = EndPiece;
+        Pieces[endTile.x, endTile.y] = StartPiece;
+    }
+    public bool IsCloseTo(Tile start, Tile end)
+    {
+        if(Math.Abs((start.x - end.x))==1 && start.y == end.y)
+                {
+            return true;
+        }
+        if(Math.Abs((start.y- end.y))==1 && start.x == end.x)   
+        {
+             return true;
+    }
+        return false;
     }
 }
